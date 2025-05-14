1e92bb8c2afd7283531344f636af81fd599cd71f
Arturiooo <arthur-nahushev@mail.ru>
2025-05-14 21:09:01 +0300

    Изменена структура, добавлен новый API

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_KEY = os.getenv("API_ID")
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+CITY_API_KEY = os.getenv("CITY_API_KEY")
 
