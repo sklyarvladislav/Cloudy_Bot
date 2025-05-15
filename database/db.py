1c94ba748072a291e94178b1f88ba16f2d4e2f24
Arturiooo <arthur-nahushev@mail.ru>
2025-05-16 00:15:51 +0300

    Добавлен блок добавления/просмотра/удаления города

--- database/db.py
+++ database/db.py
@@ -6,48 +6,65 @@ cursor = conn.cursor()
 #--- Хранение времен пользователя ---#
 
 # Добавить время
-def set_user_time(user_id: int, time: str) -> bool:
+def set_user_time_db(user_id: int, time: str) -> bool:
     conn = sqlite3.connect('bot_database.db')
     cursor = conn.cursor()
 
-    # проверяем количество уже сохранённых времён
-    cursor.execute("SELECT COUNT(*) FROM users_time WHERE user_id = ?", (user_id,))
+    # Проверка: время уже существует у пользователя?
+    cursor.execute("SELECT 1 FROM users_time WHERE user_id = ? AND time = ? LIMIT 1",
+                  (user_id, time)
+                  )
+    
+    if cursor.fetchone():
+        conn.close()
+        return False  # Время уже добавлено
+
+    # Проверка лимита (считаем только если время ещё не добавлено)
+    cursor.execute("SELECT COUNT(*) FROM users_time WHERE user_id = ?",
+                  (user_id,)
+                  )
+
     count_times = cursor.fetchone()[0]
 
-    # пользователь превысил лимит
     if count_times >= 3:
         conn.close()
-        return False
+        return False  # Лимит превышен
 
-    cursor.execute('''
-        INSERT INTO users_time(user_id, time) VALUES (?, ?)
-    ''', (user_id, time))
+    # Добавление нового времени
+    cursor.execute("INSERT INTO users_time(user_id, time) VALUES (?, ?)",
+                  (user_id, time)
+                  )
+    
     conn.commit()
     conn.close()
     return True
 
 
+
 # запросить время по id для вывода пользователю
-def get_user_time(user_id: int) -> str:
+def get_user_time_db(user_id: int) -> str:
     conn = sqlite3.connect('bot_database.db')
     cursor = conn.cursor()
+
     cursor.execute("SELECT time FROM users_time WHERE user_id = ?", (user_id,))
     times = [row[0] for row in cursor.fetchall()]
+
     conn.close()
-    
     return ', '.join(times) if times else 'отсутствует'
 
 # Запросить время по id для кнопок
-def get_user_time_list(user_id: int) -> list[str]:
+def get_user_time_btn_db(user_id: int) -> list[str]:
     conn = sqlite3.connect("bot_database.db")
     cursor = conn.cursor()
+
     cursor.execute("SELECT time FROM users_time WHERE user_id = ?", (user_id,))
     times = [row[0] for row in cursor.fetchall()]
+
     conn.close()
     return times
 
 # Удалить время
-def delete_user_time(user_id: int, time: str):
+def delete_user_time_db(user_id: int, time: str):
     conn = sqlite3.connect("bot_database.db")
     cursor = conn.cursor()
     cursor.execute("DELETE FROM users_time WHERE user_id = ? AND time = ?", (user_id, time))
@@ -58,7 +75,7 @@ def delete_user_time(user_id: int, time: str):
 #--- Хранение города для погоды ---#
 
 # Добавить город
-def set_user_city(user_id: int, city: str) -> bool:
+def set_user_city_db(user_id: int, city: str) -> bool:
     conn = sqlite3.connect("bot_database.db")
     cursor = conn.cursor()
 
@@ -71,27 +88,25 @@ def set_user_city(user_id: int, city: str) -> bool:
         conn.close()
         return False
     
-    cursor.execute(''' 
-            INSERT INTO users_city(user_id, city) VALUES (?, ?)
-    ''', (user_id, city))
+    cursor.execute("INSERT INTO users_city(user_id, city) VALUES (?, ?)", (user_id, city))
 
     conn.commit()
     conn.close()
     return True 
 
 # Вывод текущего города
-def get_user_city(user_id: int) -> str:
+def get_user_city_db(user_id: int) -> str | None:
     conn = sqlite3.connect("bot_database.db")
     cursor = conn.cursor()
 
     cursor.execute("SELECT city FROM users_city WHERE user_id = ?", (user_id,))
-    city = cursor.fetchall()
+    city = cursor.fetchone()
 
     conn.close()
-    return city if city else "отсутствует"
+    return city[0] if city else None
 
 # Удаление города 
-def delete_user_city(user_id: int, city: str):
+def delete_user_city_db(user_id: int, city: str):
     conn = sqlite3.connect("bot_database.db")
     cursor = conn.cursor()
 
