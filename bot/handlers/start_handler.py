edd174a070eaa4a097d99e63a3c9534d8fe4a93f
Arturiooo <arthur-nahushev@mail.ru>
2025-05-16 21:20:47 +0300

    Функция /start вынесена в отдельный файл из main.py

---
+++ bot/handlers/start_handler.py
@@ -0,0 +1,19 @@
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram import Router
+
+from keyboards.weather_kb import start_message_ikb
+
+start_router = Router()
+
+#--- Блок /start ---#
+@start_router.message(Command("start"))
+async def start_command(message: Message):
+
+    await message.answer(f"Привет, {message.from_user.full_name}. " \
+                         f"Чтобы узнать погоду, напишите название города или дайте доступ к геопозиции\n\n" +
+                          f"_❗️О выводе погоды по геопозиции❗️\n_" \
+                          f"*1.* Функция недоступна для ПК\n" + 
+                          f"*2.* Telegram слишком точно определяет вашу геопозицию," + 
+                          " поэтому иногда может показывать погоду не города, а вашего района",
+                           reply_markup = start_message_ikb, parse_mode = "Markdown")
