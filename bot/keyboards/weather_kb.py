49c9fb101423121d93f0986ed2d82f27c83f2ed7
Arturiooo <arthur-nahushev@mail.ru>
2025-05-16 22:12:06 +0300

    На кнопки добалены эмодзи

---
+++ bot/keyboards/weather_kb.py
@@ -0,0 +1,16 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+geolocation_kb = ReplyKeyboardMarkup(keyboard = [
+                                    [KeyboardButton(text = "📍 Отправить геопозицию", request_location=True),
+                                     KeyboardButton(text = "↩️ Отменить действие")
+                                    ]
+                                    ],
+                                     resize_keyboard=True,
+                                     one_time_keyboard=True)
+
+# кнопка для геопозиции
+start_message_ikb = InlineKeyboardMarkup(inline_keyboard=[
+                    [InlineKeyboardButton(text="Погода в моем городе 🌥️", callback_data="get_user_geo")]
+                    ])
